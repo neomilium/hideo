297631a0150e71e16cc56cacf9120a0a9dba922f
neomilium <neomilium@fb227f2e-e3d6-6c3c-9282-1afe4a6f4f62>
2007-09-11 19:20:26 +0000

    Really improved PS/2 handling!! (circular buffer for read bytes, debug code, fixed parity "What did my teachers taught me? <romain"). Current status:  - Reading seems to be okay;  - Writing not tested yet.

--- firmware/ps2.c
+++ firmware/ps2.c
@@ -25,22 +25,81 @@
 #define clock_get()     (PS2_CLK_DDR  = 0, PS2_CLK_OUT = 0, PS2_CLK_IN)
 #define clock_release() (PS2_CLK_DDR  = 0, PS2_CLK_OUT = 0)
 
+#define BUFFER_SIZE 20
+#define BUFFER_NEXT(i) ((i + 1) % BUFFER_SIZE)
+
+static volatile uint8 _buffer[BUFFER_SIZE];
+static volatile uint8 _buffer_w, _buffer_r;
 
 static volatile uint8 _data;                 /* Byte being read */
 static volatile uint8 _bit_read;             /* Number of bits already read in _data */
 static volatile uint8 _parity;               /* Received bit parity */
+static volatile uint8 _cparity;              /* Computed parity bit */
 static ps2_data_read_t _data_read;  /* Function to call when _data is complete */
 
-static enum {PS_START, PS_DATA, PS_PARITY, PS_STOP} ps2_state = PS_START;
+static volatile char _dump[] = "XXXXXXXXXXXXY\0";
+
+static volatile enum {PS_START, PS_DATA, PS_PARITY, PS_STOP} ps2_state = PS_START;
 
 /* Private members */
 inline void ps2_bit_available(void);
 inline void ps2_inhib(void);
 
+
+/**
+ * @fn ps2_buffer_write(uint8 data)
+ *
+ * @param uint8 *data Data to write to buffer.
+ *
+ * @return 1 on success, 0 otherwise.
+ */
+uint8
+ps2_buffer_write(uint8 data)
+{
+	if (BUFFER_NEXT(_buffer_w) == _buffer_r) {
+		/* Out of space ! */
+		return(0);
+	}
+	_buffer[_buffer_w] = data;
+	_buffer_w = BUFFER_NEXT(_buffer_w);
+	return(1);
+}
+
+/**
+ * @fn ps2_buffer_read(uint8 *data)
+ *
+ * @param uint8 *data Data read from buffer.
+ *
+ * @return 1 on success, 0 otherwise.
+ */
+uint8
+ps2_buffer_read(uint8 *data)
+{
+	if (_buffer_w == _buffer_r) {
+		return(0);
+	}
+
+	*data = _buffer[_buffer_r];
+	_buffer_r = BUFFER_NEXT(_buffer_r);
+
+	return(1);
+}
+
 ISR(INT2_vect) {
 	//lcd_display_string(PSTR("I"));
 	ps2_bit_available();
-	GIFR &= ~(1 << INT2);
+	GIFR &= ~(1 << INT2); /* XXX This is not used to disable interrupt !! */
+}
+
+void
+ps2_debug(void)
+{
+	char *c = _dump;
+	lcd_gotoxy(0,0);
+	while (*c) {
+		lcd_display_char(*c);
+		c++;
+	}
 }
 
 void
@@ -48,12 +107,12 @@ ps2_init(void)
 {
 	/* Set up an interrupt for calling ps2_bit_available when clk 1 -> 0 */
 	GICR   &= ~(1 << INT2);            /* Disable interrupt */
-	//ps2_write(0xFF);
 	clock_release();
 	data_release();
+	//ps2_write(0xFF, 1);
 	MCUCSR &= ~(1 << ISC2);            /* Falling edge trigger interrupt */
 	GICR   |=  (1 << INT2);            /* Enable interrupt */
-	//GIFR &= ~(1 << INT2);
+	GIFR &= ~(1 << INT2);
 }
 
 void
@@ -66,54 +125,54 @@ inline void
 ps2_bit_available(void)
 {
 	uint8 bit;
+	_dump[12] = 'i';
 
   bit = data_get();
 
 	switch (ps2_state) {
 		case PS_START:
-			//lcd_display_string(PSTR("S"));
+			_dump[0] = bit + '0';
 			if (0 == bit) {
 				/* Prepare for reading next byte */
 				_data     = 0;
 				_bit_read = 0;
-				_parity   = 0;
-
+				_cparity  = 1;
 				ps2_state = PS_DATA;
-			} else {
-				ps2_inhib();
 			}
 			break;
 		case PS_DATA:
-			//lcd_display_string(PSTR("D"));
-			_bit_read++;
 			/* Least significant bits first */
 			_data |= (bit << _bit_read);
-  		_parity ^= bit;
+			_bit_read++;
+  		_cparity ^= bit;
+			_dump[_bit_read] = bit + '0';
 			if (8 == _bit_read) {
 				ps2_state = PS_PARITY;
 			}
 			break;
 		case PS_PARITY:
-			//lcd_display_string(PSTR("P"));
-			if (bit == _parity) {
-				ps2_state = PS_STOP;
-			} else {
-				ps2_state = PS_START;
-				lcd_display_string(PSTR(" X_X "));
-				ps2_inhib();
-				ps2_write(0xFE);
-			}
+			_parity = bit;
+			_dump[9] = bit + '0';
+			ps2_state = PS_STOP;
 			break;
 		case PS_STOP:
-			//lcd_display_line(PSTR("X"));
-			if (1 == bit) {
+			_dump[10] = bit + '0';
+			if ((1 == bit) && (_parity == _cparity)) {
 				/* Send byte */
+/* XXX
 				if (_data_read) {
 					_data_read(_data);
 				}
+*/
+				ps2_buffer_write(_data);
+				_dump[11] = ' ';
 			} else {
-				ps2_inhib();
-				ps2_write(0xFE);
+				if (!bit) {
+					_dump[11] = 'S';
+				} else {
+					_dump[11] = 'P';
+				}
+				//ps2_write(0xFE, 1); /* Same player shoot again */
 			}
 			ps2_state = PS_START;
 			break;
@@ -130,22 +189,21 @@ ps2_inhib(void)
 }
 
 uint8
-ps2_write(uint8 d)
+ps2_write(uint8 d, uint8 from_int)
 {
 	uint8 current_bit = 8;
-	uint8 parity = 0;
+	uint8 parity = 1;
 	uint8 ack;
 
-// 	lcd_display_string(PSTR("W"));
 
-	GICR &= ~(1 << INT2);              /* Disable interrupt */
+	if (!from_int) {
+		GICR &= ~(1 << INT2);              /* Disable interrupt */
+	}
 
 	data_get();	
 	clock_set(0);
 	_delay_us(60);
-	_delay_us(60);
 	data_set(0);
-	_delay_us(60);
 	_delay_us(60);
 	clock_release();
 
@@ -183,7 +241,9 @@ ps2_write(uint8 d)
 
 // 	lcd_display_string(PSTR("w"));
 
-	GICR |= (1 << INT2);               /* Enable interrupt */
+	if (!from_int) {
+		GICR |= (1 << INT2);               /* Enable interrupt */
+	}
 
 	return(!ack);
 }
--- firmware/ps2.h
+++ firmware/ps2.h
@@ -5,6 +5,8 @@ typedef void (*ps2_data_read_t)(uint8 data);
 
 void ps2_init(void);
 void ps2_set_data_read(ps2_data_read_t data_read);
-uint8 ps2_write(uint8 d);
+uint8 ps2_write(uint8 d, uint8 from_int);
+uint8 ps2_buffer_read(uint8 *data);
+void ps2_debug(void);
 
 #endif /* !_PS2_H_ */
