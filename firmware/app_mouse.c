366903b3f21007659d8aaea35c1e5f35fb5385c7
neomilium <neomilium@fb227f2e-e3d6-6c3c-9282-1afe4a6f4f62>
2007-09-15 20:26:10 +0000

    - app_mouse is now the "proof of concept" for the new event handler

--- firmware/app_mouse.c
+++ firmware/app_mouse.c
@@ -6,6 +6,9 @@
 
 #include "keyboard.h"
 
+static sint16 _mouse_x = 0;
+static sint16 _mouse_y = 0;
+
 void
 _app_mouse_init(void)
 {
@@ -15,16 +18,73 @@ _app_mouse_init(void)
 void
 _app_mouse_event_handler(const event_t event)
 {
-	lcd_gotoxy(4,4);
-	lcd_display_char('n');
-	switch(event.code){
+	lcd_gotoxy(0,0);
+	lcd_display_string(PSTR("L:"));
+	lcd_gotoxy(24,0);
+	lcd_display_string(PSTR("M:"));
+	lcd_gotoxy(48,0);
+	lcd_display_string(PSTR("R:"));
+
+	lcd_gotoxy(0,2);
+	lcd_display_string(PSTR("X:"));
+	lcd_gotoxy(0,3);
+	lcd_display_string(PSTR("Y:"));
+
+	switch(event.code) {
 		case E_MOUSE_BUTTON_PRESSED:
-			lcd_gotoxy(4,4);
-			lcd_display_char('X');
-			break;
+			switch(event.data) {
+				case MOUSE_BUTTON_LEFT:
+					lcd_gotoxy(12,0);
+					lcd_display_char('1');
+				break;
+				case MOUSE_BUTTON_MIDDLE:
+					lcd_gotoxy(36,0);
+					lcd_display_char('1');
+				break;
+				case MOUSE_BUTTON_RIGHT:
+					lcd_gotoxy(60,0);
+					lcd_display_char('1');
+				break;
+			}
+		break;
 		case E_MOUSE_BUTTON_RELEASED:
-			lcd_gotoxy(4,4);
-			lcd_display_char(' ');
-			break;
+			switch(event.data) {
+				case MOUSE_BUTTON_LEFT:
+					lcd_gotoxy(12,0);
+					lcd_display_char('0');
+				break;
+				case MOUSE_BUTTON_MIDDLE:
+					lcd_gotoxy(36,0);
+					lcd_display_char('0');
+				break;
+				case MOUSE_BUTTON_RIGHT:
+					lcd_gotoxy(60,0);
+					lcd_display_char('0');
+				break;
+			}
+		break;
+		case E_MOUSE_X_REV:
+			_mouse_x += event.data;
+			lcd_gotoxy(12,2);
+			lcd_display_number(_mouse_x);
+			lcd_finish_line();
+		break;
+		case E_MOUSE_X_FWD:
+			_mouse_x -= event.data;
+			lcd_gotoxy(12,2);
+			lcd_display_number(_mouse_x);
+			lcd_finish_line();
+		break;
+		case E_MOUSE_Y_REV:
+			_mouse_y += event.data;
+			lcd_gotoxy(12,3);
+			lcd_display_number(_mouse_y);
+			lcd_finish_line();
+		break;
+		case E_MOUSE_Y_FWD:
+			_mouse_y -= event.data;
+			lcd_gotoxy(12,3);
+			lcd_display_number(_mouse_y);
+		break;
 	}
 }
