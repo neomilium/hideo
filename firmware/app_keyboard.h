d7b67d90ff245df6c0a8b6e0472b77573732e68b
neomilium <neomilium@fb227f2e-e3d6-6c3c-9282-1afe4a6f4f62>
2008-03-31 12:16:41 +0000

    Add multiple event-handlers capabilities. Rename event.* become eventmanager.*

--- firmware/app_keyboard.h
+++ firmware/app_keyboard.h
@@ -2,7 +2,7 @@
 #define __APP_KEYBOARD_H__
 
 #include "application.h"
-#include "event.h"
+#include "eventmanager.h"
 
 #define app_keyboard_init()	application_t app_keyboard = { &_app_keyboard_init, &_app_keyboard_event_handler, NULL }
 
--- firmware/app_motor.h
+++ firmware/app_motor.h
@@ -2,7 +2,7 @@
 #define _APP_MOTOR_H_
 
 #include "application.h"
-#include "event.h"
+#include "eventmanager.h"
 
 #define app_motor_init()	application_t app_motor = { &_app_motor_init, &_app_motor_event_handler, NULL }
 
