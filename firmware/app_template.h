ce019fd076d02f1f620a8eb2b1800cc07c73b574
neomilium <neomilium@fb227f2e-e3d6-6c3c-9282-1afe4a6f4f62>
2008-04-19 22:22:21 +0000

    Add template for creating new application.

---
+++ firmware/app_template.h
@@ -0,0 +1,13 @@
+#ifndef _APP_TEMPLATE_H_
+#define _APP_TEMPLATE_H_
+
+#include "application.h"
+#include "eventmanager.h"
+
+#define app_template_init()	application_t app_template = { &_app_template_init, &_app_template_event_handler, NULL }
+
+
+void _app_template_init(void*);
+void _app_template_event_handler(const event_t event);
+
+#endif /* !_APP_TEMPLATE_H_ */
