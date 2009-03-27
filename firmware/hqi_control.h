989bed66bdd69383a81b6e0c6abd7855db004b72
neomilium <neomilium@fb227f2e-e3d6-6c3c-9282-1afe4a6f4f62>
2009-03-27 23:12:45 +0000

    Decrease DC motor's max speed, should be better to stabilise lens when moving to a position. Create hqi_control.* to handle HQI's status and cooling. Dirty application tweaking on app_hqi and app_poweroff to use new hqi_control.

---
+++ firmware/hqi_control.h
@@ -0,0 +1,18 @@
+#ifndef _HQI_CONTROL_H_
+#define _HQI_CONTROL_H_
+
+#include "types.h"
+
+typedef enum {
+	HQI_MODE_READY,
+	HQI_MODE_RUNNING,
+	HQI_MODE_COOLING,
+}		hqi_mode_t;
+
+void		hqi_init (void);
+void		hqi_start(void);
+void		hqi_stop(void);
+hqi_mode_t	hqi_mode(void);
+uint16		hqi_remaining_time_before_ready(void);
+
+#endif /* ! _HQI_CONTROL_H_ */
