e58b36b66e2b60b4c771ca822993f6a172f9c1bf
neomilium <neomilium@fb227f2e-e3d6-6c3c-9282-1afe4a6f4f62>
2009-03-28 00:18:13 +0000

    Add start and stop functions to rtc to be able to control RTC clock enable. Add lifetime counter related function to hqi_control.

--- firmware/drv_ds1307.c
+++ firmware/drv_ds1307.c
@@ -9,17 +9,37 @@ void
 drv_ds1307_init(void)
 {
 	byte		data      [2];
+
+	data[0] = 0x07; // Register address in DS1307
+	data[1] = 0x00;
+
+	/* Clear OUT, SQWE, RS1, RS0 */
+	i2c_master_send_ni(DS1307_I2C_ADDR, 2, (byte*)&data);
+}
+
+void
+drv_ds1307_start(void)
+{
+	byte		data      [2];
 	data[0] = 0x00;
 	/* Timekeeper Registers address */
 	i2c_master_send_ni(DS1307_I2C_ADDR, 1, &data[0]);
 	i2c_master_receive_ni(DS1307_I2C_ADDR, 1, &data[1]);
 	data[1] &= 0x7F;
-	/* Clear CH(enable clock generator) */
+	/* Clear CH (enable clock generator) */
 	i2c_master_send_ni(DS1307_I2C_ADDR, 2, (byte*)&data);
+}
 
-	data[0] = 0x07;
-	data[1] = 0x00;
-	/* Clear OUT, SQWE, RS1, RS0 */
+void
+drv_ds1307_stop(void)
+{
+	byte		data      [2];
+	data[0] = 0x00;
+	/* Timekeeper Registers address */
+	i2c_master_send_ni(DS1307_I2C_ADDR, 1, &data[0]);
+	i2c_master_receive_ni(DS1307_I2C_ADDR, 1, &data[1]);
+	data[1] |= 0x80;
+	/* Set CH (disable clock generator) */
 	i2c_master_send_ni(DS1307_I2C_ADDR, 2, (byte*)&data);
 }
 
@@ -30,20 +50,30 @@ drv_ds1307_read(void)
 	i2c_master_send_ni(DS1307_I2C_ADDR, 1, ds1307_addr);
 	rtc_datetime_t	data;
 	i2c_master_receive_ni(DS1307_I2C_ADDR, sizeof(rtc_datetime_t), (byte *) (&data));
+	data.seconds &= 0x7F;
 	data.hours &= 0x3F;
 	return data;
 }
 
 void
-drv_ds1307_write(const rtc_datetime_t datetime)
+drv_ds1307_write(const rtc_datetime_t rtc_datetime)
 {
 	struct {
 		byte		address;
 		rtc_datetime_t	datetime;
 	}		data;
 
+
 	data.address = 0x00;
-	data.datetime = datetime;
+	data.datetime = rtc_datetime;
+
+	/* Keep CH (clock enable) bit as it set */
+	byte		ch;
+	/* Timekeeper Registers address */
+	i2c_master_send_ni(DS1307_I2C_ADDR, 1, 0x00);
+	i2c_master_receive_ni(DS1307_I2C_ADDR, 1, &ch);
+	ch &= 0x7F;
+	data.datetime.seconds |= ch;
 
 	i2c_master_send_ni(DS1307_I2C_ADDR, sizeof(data), (byte*)&data);
 }
--- firmware/drv_ds1307.h
+++ firmware/drv_ds1307.h
@@ -5,9 +5,12 @@
 #include "rtc.h"
 
 void		drv_ds1307_init(void);
-/* void ds1307_write(uint8 data); */
 rtc_datetime_t	drv_ds1307_read(void);
+void		drv_ds1307_write(const rtc_datetime_t rtc_datetime);
+
 /* void ds1307_write_data( X, Y ) */
+void		drv_ds1307_start(void);
+void		drv_ds1307_stop(void);
 /* ds1307_read_data( X ) */
 /* ds1307_read_datetime() */
 
