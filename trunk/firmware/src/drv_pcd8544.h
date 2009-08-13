ca9620457240eeefa1848bcc2229c06b26ec72c5
neomilium <neomilium@fb227f2e-e3d6-6c3c-9282-1afe4a6f4f62>
2009-08-13 13:39:38 +0000

    Fix #20: pcd8544 related is now correctly named.

---
+++ trunk/firmware/src/drv_pcd8544.h
@@ -0,0 +1,34 @@
+#ifndef __LCDNOKIA_H_DRIVER__
+#define __LCDNOKIA_H_DRIVER__
+
+#include <avr/io.h>
+#include "hideo.h"
+
+#define _DRV_NOKIA_DDR			DDRA
+#define _DRV_NOKIA_MASK			0b00011111
+
+#define PCD8544_SCLK    GET_BIT(PORTA).bit4		/* pcd8544 lcd sclk */
+#define PCD8544_SDA     GET_BIT(PORTA).bit3		/* pcd8544 lcd sda */
+#define PCD8544_DC      GET_BIT(PORTA).bit2		/* pcd8544 lcd d/!c	 */
+#define PCD8544_CS      GET_BIT(PORTA).bit1		/* pcd8544 lcd !cs */
+#define PCD8544_RES     GET_BIT(PORTA).bit0		/* pcd8544 lcd !res */
+
+#define PCD8544_SCREEN_WIDTH 84
+
+#include <stdint.h>
+
+/* Fonctions qui concernent les manipulations hardware (ceci est le driver) */
+void		pcd8544_init(void);
+void		pcd8544_reset_DDRAM(void);
+void		pcd8544_write(uint8_t data);
+void		pcd8544_send_command(const uint8_t command);
+void		pcd8544_send_data(const uint8_t data);
+
+/* Fonctions qui gerent l'affichage (API : Application Programming Interface) */
+void		pcd8544_clear(void);
+void		pcd8544_gotoxy(const uint8_t x, const uint8_t y);
+void		pcd8544_display_char(const uint8_t ascii);
+void		pcd8544_display_string(const char *string);
+void		pcd8544_set_mode(uint8_t mode);
+void		pcd8544_finish_line(void);
+#endif
--- trunk/firmware/src/lcd.h
+++ trunk/firmware/src/lcd.h
@@ -2,20 +2,20 @@
 #define __LCD_H__
 
 #include <avr/pgmspace.h>
-#include "drv_nokia.h"
+#include "drv_pcd8544.h"
 
-#define lcd_init() nokia_init()
-#define lcd_clear() nokia_clear()
-#define lcd_display_char( X ) nokia_display_char( X )
-#define lcd_display_string( X ) nokia_display_string( X )
-#define lcd_gotoxy( X, Y ) nokia_gotoxy( X, Y )
-#define lcd_finish_line() nokia_finish_line()
+#define lcd_init() pcd8544_init()
+#define lcd_clear() pcd8544_clear()
+#define lcd_display_char( X ) pcd8544_display_char( X )
+#define lcd_display_string( X ) pcd8544_display_string( X )
+#define lcd_gotoxy( X, Y ) pcd8544_gotoxy( X, Y )
+#define lcd_finish_line() pcd8544_finish_line()
 
 #define LCD_MODE_NORMAL   0
 #define LCD_MODE_INVERTED 1
-#define lcd_set_mode(mode) nokia_set_mode(mode)
+#define lcd_set_mode(mode) pcd8544_set_mode(mode)
 
-#define lcd_display_line( X ) nokia_display_string( X ); nokia_finish_line()
+#define lcd_display_line( X ) pcd8544_display_string( X ); pcd8544_finish_line()
 
 void		lcd_display_number(int16_t number);
 void		lcd_display_hex(const uint8_t hex);
